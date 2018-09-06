404958c1a1494d7866182a3b39ad806c32118d12
Nye Liu <nye@blockdaemon.com>
2018-09-06 03:56:39 +0000

    Moved script to tools, removed userName patter

--- tools/munge-upstream.sh
+++ tools/munge-upstream.sh
@@ -2,6 +2,5 @@
 sed -i "
 s/heroes-service-network/{{env['NETWORK'] or \"some-network\"}}/
 s/chainhero.io/{{env['DOMAIN'] or \"localhost\"}}/
-s/{userName}/Admin/
 s#\${GOPATH}/src/github.com/chainHero/heroes-service/fixtures/crypto-config#{{env['CRYPTO']}}#
 " $1
